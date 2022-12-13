86fa9ad2026721ac72cd37959629835fc7a5a414
Mark Arranz <mark.arranz@gmail.com>
2022-12-13 09:15:57 -0800

    Add ability to set # of top elves to calculate.

--- 2022/01/main.py
+++ 2022/01/main.py
@@ -1,10 +1,33 @@
-max_calories = 0
+from typing import List
+
+def adjust_max_calories(max_calories: List[int], cur_calories: int) -> None:
+    for i, v in enumerate(max_calories):
+        if (v < cur_calories):
+            max_calories.insert(i, cur_calories)
+            max_calories.pop()
+            return
+    return
+
+max_size = int(input("Find the top __ Elves carrying the most Calories.\nEnter a number: "))
+
+max_calories: List[int] = []
 cur_calories = 0
+
 with open('./input.txt', 'r') as file:
     while (line := file.readline()) != '':
-        if line == '\n':
-            max_calories = max(max_calories, cur_calories)
-            cur_calories = 0
-        else:
+        if line != '\n':
             cur_calories += int(line.rstrip())
-print(max(max_calories, cur_calories))
+            continue
+
+        if len(max_calories) < max_size:
+            max_calories.append(cur_calories)
+            cur_calories = 0
+            continue
+
+        adjust_max_calories(max_calories, cur_calories)
+        cur_calories = 0
+
+
+adjust_max_calories(max_calories, cur_calories)
+
+print(sum(max_calories))
