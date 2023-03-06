3af84d7f990a43f2857fec3fa60cf791645def45
Mark Arranz <mark.arranz@gmail.com>
2023-03-05 23:17:05 -0800

    Completed 06

---
+++ 2022/06/main.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+
+def find_start_of_packet_marker() -> int:
+    with open("./input.txt") as f:
+        stream = f.readline()
+    unique_needed = 14
+    marker = deque(maxlen=unique_needed)
+    for count, char in enumerate(stream, 1):
+        while char in marker:
+            marker.popleft()
+        marker.append(char)
+        if len(marker) == unique_needed:
+            return count
+
+
+print(find_start_of_packet_marker())
